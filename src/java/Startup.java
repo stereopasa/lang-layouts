6901c823265982e320dacb3be02fcf1bb67b8a59
pasa <stereopasa@icloud.com>
2017-10-03 19:31:41 +0300

    works

--- src/java/Startup.java
+++ src/java/Startup.java
@@ -1,14 +1,11 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.google.gson.*;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Startup {
 
@@ -19,58 +16,70 @@ public class Startup {
     }
 
     class LayoutVO {
-        String layouName;
+        String layoutName;
         String[] languages;
         String type;
 
-        public LayoutVO(String layouName, String[] languages, String type) {
-            this.layouName = layouName;
+        public LayoutVO(String layoutName, String[] languages, String type) {
+            this.layoutName = layoutName;
             this.languages = languages;
             this.type = type;
         }
 
-        JSONObject getJSONObject() {
-            JSONObject res = new JSONObject();
-            res.put("layout", layouName);
-            res.put("lang", Arrays.toString(languages));
-            res.put("type", type);
+        JsonObject getJSONObject() {
+            JsonObject res = new JsonObject();
+            res.addProperty("layout", layoutName);
+            res.add("lang", new JsonArray());
+            res.addProperty("type", type);
 
             return res;
         }
     }
 
     class Visitor extends SimpleFileVisitor<Path> {
-        private JSONParser parser = new JSONParser();
+        private JsonParser parser = new JsonParser();
+        private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
 
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
             if (file.getFileName().toString().equals("config.json")) {
                 System.out.println(file.toAbsolutePath());
-                try (Reader reader = Files.newBufferedReader(file)) {
-                    JSONObject data = (JSONObject) parser.parse(reader);
-                    System.out.println(data);
+                String fileString = new String(Files.readAllBytes(file));
 
-                    JSONArray layouts = (JSONArray) data.get("layouts");
-                    if (layouts != null) {
-                        JSONArray newLayouts = new JSONArray();
-                        for (Object layout : layouts) {
-                            String layoutName = (String) layout;
+                Pattern pattern = Pattern.compile("\\s+\"layouts\": \\[(\\s|.)+?\\],");
+                Matcher matcher = pattern.matcher(fileString);
+                if (matcher.find()) {
+                    int start = matcher.start();
+                    int end = matcher.end();
+                    String layoutsDataString = matcher.group();
+                    String jsonString = "{" + layoutsDataString.substring(0, layoutsDataString.lastIndexOf(',')) + "}";
+                    JsonObject jsonObject = parser.parse(jsonString).getAsJsonObject();
+                    JsonElement layoutsElement = jsonObject.get("layouts");
+                    if (layoutsElement != null) {
+                        JsonArray layouts = new JsonArray();
+                        for (JsonElement layout : layoutsElement.getAsJsonArray()) {
+                            String layoutName = layout.getAsString();
                             LayoutVO vo = new LayoutVO(layoutName, new String[]{}, "game");
-                            newLayouts.add(vo.getJSONObject());
+                            layouts.add(vo.getJSONObject());
                         }
 
-                        if (newLayouts != null) {
-                            data.replace("layouts", newLayouts);
-                            System.out.println(data);
+                        if (layouts != null) {
+                            jsonObject.add("layouts", layouts);
+                            String prettyJson = gson.toJson(jsonObject);
+
+                            int startPretty = prettyJson.indexOf('"');
+                            int endPretty = prettyJson.lastIndexOf(']') + 1;
+                            String writeJson = prettyJson.substring(startPretty, endPretty);
+                            String resultString = fileString.substring(0, start)
+                                    + System.getProperty("line.separator") + ' ' + writeJson + ","
+                                    + fileString.substring(end);
+
+                            try (Writer writer = Files.newBufferedWriter(file.getParent().resolve("res.json"))) {
+                                writer.write(resultString);
+                            }
                         }
                     }
-
-                    try (Writer writer = Files.newBufferedWriter(file.getParent().resolve("res.json"))) {
-                        writer.write(data.toJSONString());
-                    }
-
-                } catch (IOException | ParseException e) {
-                    e.printStackTrace();
                 }
                 return FileVisitResult.SKIP_SUBTREE;
             }
